ccf0a431733b3e15cf5997bfb5c16607a53bba8c
a.changizi <A2developmentaccess@gmail.com>
2022-08-29 19:02:53 +0430

    - Testing - FakeBorutoApi.kt

---
+++ app/src/test/java/com/tojare/borutoapp/data/remote/FakeBorutoApi.kt
@@ -0,0 +1,96 @@
+package com.tojare.borutoapp.data.remote
+
+import com.tojare.borutoapp.domain.model.ApiResponse
+import com.tojare.borutoapp.domain.model.Hero
+
+class FakeBorutoApi : BorutoApi {
+
+    private val heroes = listOf(
+        Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "/images/sasuke.jpg",
+            about = "Sasuke Uchiha is one of the last surviving members of Konohagakure's Uchiha clan. After his older brother, Itachi, slaughtered their clan, Sasuke made it his mission in life to avenge them by killing Itachi. He is added to Team 7 upon becoming a ninja and, through competition with his rival and best friend, Naruto Uzumaki.",
+            rating = 5.0,
+            power = 98,
+            month = "July",
+            day = "23rd",
+            family = listOf(
+                "Fugaku",
+                "Mikoto",
+                "Itachi",
+                "Sarada",
+                "Sakura"
+            ),
+            abilities = listOf(
+                "Sharingan",
+                "Rinnegan",
+                "Intelligence"
+            ),
+            natureTypes = listOf(
+                "Lightning",
+                "Fire",
+                "Wind",
+                "Earth",
+                "Water"
+            )
+        ),
+        Hero(
+            id = 2,
+            name = "Naruto",
+            image = "/images/naruto.jpg",
+            about = "Naruto Uzumaki is one of the last surviving members of Konohagakure's Uchiha clan. After his older brother, Itachi, slaughtered their clan, Sasuke made it his mission in life to avenge them by killing Itachi. He is added to Team 7 upon becoming a ninja and, through competition with his rival and best friend, Naruto Uzumaki.",
+            rating = 5.0,
+            power = 98,
+            month = "Oct",
+            day = "10th ",
+            family = listOf(
+                "Minato",
+                "Kushina",
+                "Bortuo",
+                "Himawari",
+                "Hinata"
+            ),
+            abilities = listOf(
+                "Rasengan",
+                "Rasen-Shuriken",
+                "Senin Mode"
+            ),
+            natureTypes = listOf(
+                "Wind",
+                "Earth",
+                "Lava",
+                "Fire",
+            )
+        ),
+    )
+
+    override suspend fun getAllHeroes(page: Int): ApiResponse {
+        return ApiResponse(
+            success = false
+        )
+    }
+
+    override suspend fun searchHeroes(name: String): ApiResponse {
+        val searchedHeroes = findHeroes(name = name)
+        return ApiResponse(
+            success = true,
+            message = "ok",
+            heroes = searchedHeroes
+        )
+    }
+
+    private fun findHeroes(name: String): List<Hero> {
+        val founded = mutableListOf<Hero>()
+        return if (name.isNotEmpty()) {
+            heroes.forEach { hero ->
+                if (hero.name.lowercase().contains(name.lowercase())) {
+                    founded.add(hero)
+                }
+            }
+            founded
+        } else {
+            emptyList()
+        }
+    }
+}
